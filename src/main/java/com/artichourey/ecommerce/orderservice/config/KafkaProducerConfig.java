e4dd09db9992d9921b7364d2a712b8097a9fd307
Arti <arti.chourey21@gmail.com>
2026-04-07 13:59:25 +0530

    kafka: improve producer setup and serialization config

--- src/main/java/com/artichourey/ecommerce/orderservice/config/KafkaProducerConfig.java
+++ src/main/java/com/artichourey/ecommerce/orderservice/config/KafkaProducerConfig.java
@@ -17,13 +17,16 @@ public class KafkaProducerConfig {
 
 	@Bean
 	public ProducerFactory<String, Object> producerFactory() {
-	    Map<String, Object> configProps = new HashMap<>();
-	    configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-	    configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-	    configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
-	    configProps.put(JsonSerializer.ADD_TYPE_INFO_HEADERS, true); // ensures type headers are sent
-	    return new DefaultKafkaProducerFactory<>(configProps);
-	}
+		 Map<String, Object> configProps = new HashMap<>();
+		    configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+		    configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+		    configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+		    configProps.put(JsonSerializer.ADD_TYPE_INFO_HEADERS, true);
+		    configProps.put(ProducerConfig.ACKS_CONFIG, "all");
+		    configProps.put(ProducerConfig.RETRIES_CONFIG, 5);
+		    configProps.put(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, true);
+		    return new DefaultKafkaProducerFactory<>(configProps);
+		}
 
 	@Bean
 	public KafkaTemplate<String, Object> kafkaTemplate() {
